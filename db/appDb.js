eb8805d1b6b590b89a6071fe0fa9de2959f09b94
SankaUdeshika <sankaudeshika123@gmail.com>
2026-03-19 13:09:44 +0530

    Fixed Singleton Database Connection

--- db/appDb.js
+++ db/appDb.js
@@ -3,12 +3,12 @@ const mysql = require("mysql2");
 
 function getAppDb(dbName) {
   const db = mysql.createConnection({
-    host: "31.97.61.250", // VpS host
-    user: "sanka", // VPS username
-    password: "Sanka123!@", // VPS password
-    // host: "localhost",
-    // user: "root",
-    // password: "12345678",
+    // host: "31.97.61.250", // VpS host
+    // user: "sanka", // VPS username
+    // password: "Sanka123!@", // VPS password
+    host: "localhost",
+    user: "root",
+    password: "12345678",
     database: dbName, // ✅ dynamic per user
   });
 
