3f6731a046d0386d106d01eaf402bbb5764b6739
SankaUdeshika <sankaudeshika123@gmail.com>
2026-03-24 21:29:51 +0530

    Change Hosting DB

--- db/userDb.js
+++ db/userDb.js
@@ -3,12 +3,12 @@ const mysql = require("mysql2");
 
 function getUserDb() {
   const db = mysql.createConnection({
-    // host: "31.97.61.250", // VpS host
-    // user: "sanka", // VPS username
-    // password: "Sanka123!@", // VPS password
-    host: "localhost",
-    user: "root",
-    password: "12345678",
+    host: "31.97.61.250", // VpS host
+    user: "sanka", // VPS username
+    password: "Sanka123!@", // VPS password
+    // host: "localhost",
+    // user: "root",
+    // password: "12345678",
     database: "optielite_users",
   });
 
